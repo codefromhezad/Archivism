8fd8bf4889f260b715e6aae142970a4988020ef6
Pierrick Varin <pierrick.varin@glitchstudio.com>
2015-01-16 00:26:01 +0100

    HTML/CSS Base Architecturing of the home

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -1,15 +1,19 @@
 @extends('layout')
 
 @section('content')
-	<ul class="items-index">
-		@foreach($items as $item)
-			<li class="arch-item">
-				<h3>{{ $item->name }}</h3>
-				<div class="item-metas">
-					<span class="item-date">Added on {{ $item->created_at }}</span>
-					<a class="item-goto" href="{{ $item->href }}" target="_blank">See more on <em>{{ ArchProvider::$providers[$item->provider] }}</em></a>
-				</div>
-			</li>
-		@endforeach
-	</ul>
+	<div class="center-container">
+		<ul class="items-index">
+			@foreach($items as $item)
+				<li class="arch-item">
+					<div class="inner-item">
+						<h3>{{ $item->name }}</h3>
+						<div class="item-metas">
+							<span class="item-date">Added {{ $item->created_at }}</span>
+							<a class="item-goto" href="{{ $item->href }}" target="_blank">See more on <em>{{ ArchProvider::$providers[$item->provider] }}</em></a>
+						</div>
+					</div>
+				</li>
+			@endforeach
+		</ul>
+	</div>
 @stop
